0b79b65b8be87fdb3035e03949cdb42cff2ca12a
RielJ <bulaybulay.rielj@gmail.com>
2024-06-02 16:24:15 +0800

    refactor: fetchTokenInfo and hook page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,49 +1,44 @@
 <script lang="ts">
+	import { fetchTokenInfo } from './fetch-token-info';
 	import {
 		Connection,
-		PublicKey,
 		clusterApiUrl,
 		type AccountInfo,
 		type ParsedAccountData
 	} from '@solana/web3.js';
+
 	let tokenAddress = $state('');
-
-	let tokenInfo: AccountInfo<Buffer | ParsedAccountData> | null = $state(null);
 	let isLoading = $state(false);
+	let tokenInfo: AccountInfo<Buffer | ParsedAccountData> | null = $state(null);
 	let error: string | undefined = $state();
-
 	const connection = new Connection(clusterApiUrl('devnet'));
 
-	const fetchTokenInfo = async ({ tokenAddress }: { tokenAddress: string }) => {
+	const fetchToken = async () => {
+		error = undefined;
 		isLoading = true;
-		try {
-			const publicKey = new PublicKey(tokenAddress);
-			const tokenInfoRes = await connection.getParsedAccountInfo(publicKey);
-			tokenInfo = tokenInfoRes.value;
-			error = undefined;
-		} catch (err) {
-			if (err instanceof Error) {
-				error = 'Error fetching token info: ' + err.message;
-			} else {
-				error = 'Error fetching token info';
-			}
-		} finally {
+		const { err, data } = await fetchTokenInfo({ tokenAddress, connection });
+		if (err) {
+			error = err;
 			isLoading = false;
+			return;
 		}
+		tokenInfo = data;
+		isLoading = false;
 	};
 </script>
 
 <div>
 	<h1>Solana Token Info</h1>
 	<input type="text" placeholder="Enter token address" bind:value={tokenAddress} />
-	<button onclick={() => fetchTokenInfo({ tokenAddress })}>Get Token Info</button>
+	<button onclick={() => fetchToken()}>Get Token Info</button>
 	{#if isLoading}
 		<p>Loading...</p>
-	{:else if error}
+	{:else if error !== undefined}
 		<p>{error}</p>
 	{:else if tokenInfo}
-		<p>{JSON.stringify(tokenInfo, null, 4)}</p>
+		<p>{JSON.stringify(tokenInfo, null, 2)}</p>
 	{/if}
 </div>
 
 <style></style>
+
